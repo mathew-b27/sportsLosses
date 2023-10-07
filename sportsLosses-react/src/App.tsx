4c148c4f0a502c0b838251163d465a240d18a53e
AndyW1016 <andyw101604@gmail.com>
2023-10-07 01:23:19 -0400

    center

--- sportsLosses-react/src/App.tsx
+++ sportsLosses-react/src/App.tsx
@@ -1,9 +1,11 @@
 import Josh from "./components/josh";
+import Bball from "./components/andy";
 
 function App() {
   return (
     <div>
       <Josh />
+      <Bball />
     </div>
   );
 }
---
+++ sportsLosses-react/src/components/andy.tsx
@@ -0,0 +1,11 @@
+import './andystyle.css';
+function bball() {
+    return (
+      <>
+        <h1>UCF Basketball</h1>
+      </>
+    );
+  }
+  
+  export default bball;
+
